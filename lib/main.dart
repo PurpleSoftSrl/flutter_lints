d3188ae4ae110f64d0962840417ddaf640c9afe4
Simone Stasi <simone.stasi@purplesoft.io>
2023-05-25 19:03:11 +0200

    add some rules

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,1 @@
-void main(List<String> args) {
-  // final a = '1' == 1;
-
-  // final b = [
-  //   1,
-  //   ...[1, 2, 3].map((e) => e).toList(),
-  // ];
-}
+void main(List<String> args) async {}
